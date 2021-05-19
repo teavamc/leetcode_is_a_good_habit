a5c345ccb3e89de16840edc5ace715a64bdecdf7
teavamc <teavamc@163.com>
2021-05-19 11:46:37 +0800

    feat: 二叉树的中序遍历的递归解法

---
+++ code_record/src/main/java /leetcode/editor/cn/TreeNode.java
@@ -0,0 +1,18 @@
+package leetcode.editor.cn;
+
+/**
+ * @Package leetcode.editor.cn
+ * @date 2021/5/19 11:30
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
